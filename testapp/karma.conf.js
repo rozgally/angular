08ba36420052251770297b668b7b000f08bced41
rozgally <saigeetha.sabbineni@gmail.com>
2016-03-04 15:04:54 -0500

    Added the jasmine unit test configuration
    Added the jasmine unit test configuration

--- testapp/karma.conf.js
+++ testapp/karma.conf.js
@@ -1,33 +1,59 @@
 module.exports = function(config){
   config.set({
 
-    basePath : './',
+    basePath : '',
+
+    frameworks:['jasmine'],
 
     files : [
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/components/**/*.js',
-      'app/view*/**/*.js'
+      'app/*.js',
+      'app/controllers/*.js',
+      'app/services/*.js',
+      'app/directives/*.js',
+      'app/test/*.js'
     ],
-
+    preprocessors:{
+      'app/partials/*.html':'ng-html2js',
+      'app/controllers/*.js':'coverage',
+      'app/services/*.js':'coverage',
+      'app/directives/*.js':'coverage'
+    },
+    ngHtml2JsPreprocessor:{
+      stripPrefix:'app/',
+      moduleName:'directive-templates'
+    },
     autoWatch : true,
-
-    frameworks: ['jasmine'],
-
-    browsers : ['Chrome'],
 
     plugins : [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
             'karma-jasmine',
-            'karma-junit-reporter'
+            'karma-junit-reporter',
+            'karma-ng-html2js-preprocessor',
+            'karma-htmlfile-reporter',
+            'karma-coverage'
             ],
+    reporters:['progress','coverage','html'],
+    htmlReporter:{
+        outputFile: 'target/karma-reports/unit-tests-report.html',
+        //optional
+        pageTitle:'Unit Tests',
+        subPageTitle:'Report'
+    },
+    coverageReporter:{
+      reporters:[{type:'html'}],
+      dir:'coverage/'
+    },
+    port:9876,
 
-    junitReporter : {
-      outputFile: 'test_out/unit.xml',
-      suite: 'unit'
-    }
+    colors:true,
+    logLevel:config.LOG_INFO,
+    browsers:['Chrome'],
+    singleRun:false
 
-  });
-};
+  })
+}
